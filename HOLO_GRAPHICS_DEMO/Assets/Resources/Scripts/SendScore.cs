32f77c96cab3261598a40940e598e0a91012c21c
Adrian Ollero <adrian.ollero@alu.uclm.es>
2019-04-22 00:51:47 +0200

    Score is shown

--- HOLO_GRAPHICS_DEMO/Assets/Resources/Scripts/SendScore.cs
+++ HOLO_GRAPHICS_DEMO/Assets/Resources/Scripts/SendScore.cs
@@ -4,11 +4,17 @@ using UnityEngine;
 
 public class SendScore : MonoBehaviour
 {
+
     void OnTriggerEnter(Collider c)
-
     {
+        if(c.gameObject.name=="1" || c.gameObject.name == "2" || c.gameObject.name == "3"
+            || c.gameObject.name == "4" || c.gameObject.name == "5")
+        {
+            TargetManager manager = c.gameObject.transform.parent.GetComponent<TargetManager>();
+            manager.SendScore(c.gameObject.name);
 
-        Debug.Log("Ha entrado " + c.gameObject.name);
+            
+        }
 
     }
 }
--- HOLO_GRAPHICS_DEMO/Assets/Resources/Scripts/TargetManager.cs
+++ HOLO_GRAPHICS_DEMO/Assets/Resources/Scripts/TargetManager.cs
@@ -2,14 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System;
 
 public class TargetManager : MonoBehaviour
 {
-    public Text grollars_score;
+    public TextMesh grollars_score;
     public Text winner_text;
 
-    public void SendScore()
+    public void SendScore(string name)
     {
-
+        Debug.Log("Collision: "+ name);
+        int score = Convert.ToInt32(name) + Convert.ToInt32(grollars_score.text);
+        grollars_score.text = "" + score;
     }
+
+
 }
